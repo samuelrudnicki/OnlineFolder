5021fed037298d864de2861856aeca6e92b32566
Victor Piccoli <victorpicc@gmail.com>
2019-04-22 18:46:40 -0300

    Handling closure of socket

--- src/server/main.c
+++ src/server/main.c
@@ -51,6 +51,7 @@ int main(int argc, char *argv[])
 		}
 
 		printf("Handler Assigned\n");
+		
 	}
 		
 	close(sockfd);
--- src/server/server.c
+++ src/server/server.c
@@ -13,8 +13,7 @@ void *handleConnection(void *socketDescriptor) {
     char buffer[PACKET_SIZE];
     int exitCommand = FALSE;
     int n;
-    int newsockfd = *(int*)socketDescriptor;
-    
+    int newsockfd = *(int*)socketDescriptor;    
 
 	while(exitCommand == FALSE) {
         bzero(buffer, PACKET_SIZE);
@@ -22,19 +21,27 @@ void *handleConnection(void *socketDescriptor) {
         n = read(newsockfd, buffer, PACKET_SIZE);
         if (n < 0) 
             printf("ERROR reading from socket");
-        printf("Here is the Command: %s\n", buffer);
-        
-        /* write in the socket */ 
+        printf("Socket %d - Command: %s\n", newsockfd, buffer);
+
+        if(n == 0) {
+            printf("Empty Response.\n");
+            exitCommand = TRUE;
+        }
+
+        /* write in the socket */
         n = write(newsockfd,"Executing Command...", 22);
         if (n < 0) 
             printf("ERROR writing to socket");
 
+
+        
         if(strcmp(buffer,"exit\n") == 0) {
             exitCommand = TRUE;
         }
     
     }
 
+    printf("Closing Socket %d.\n", newsockfd);
 	close(newsockfd);
     return 0;
 }
